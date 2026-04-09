83f80e3a65e0dc7b992d9e0a4fa1b2a6c43a4036
Estagiario TI <estagiario.ti@toledodist.local>
2026-04-09 14:43:46 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import logo from './images/logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header ClassName="App-header">
+        <div className="logo">
+          <img src={logo} alt='logo'></img>
+          <p><strong>Alura</strong> Books</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
