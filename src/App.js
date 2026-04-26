a727b31670f09b1fbbd3ff401e93d8a7d70d0895
Igor <igorcavion123@gmail.com>
2026-04-26 20:40:27 -0300

    styled-components-global

--- src/App.js
+++ src/App.js
@@ -6,10 +6,6 @@ const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
   background-image: linear-gradient(90deg,#002F52 35%,#326589 165%);
-
-li {
-    list-style: none;    
-}
 `
 
 function App() {
